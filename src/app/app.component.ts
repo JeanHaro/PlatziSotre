b364d5c4d02d6879e8cedc75321a711a4c9d779b
Jean Haro <JeanCHL98@hotmail.com>
2022-02-03 13:28:11 -0500

    Componente para producto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { Product } from './models/product.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,6 +9,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   imgParent = '';
+  // Array de productos
+  products: Product[] = [
+    {
+      id: '1',
+      name: 'EL mejor juguete',
+      price: 565,
+      image: '../assets/img/toy.jpg'
+    },
+    {
+      id: '2',
+      name: 'Bicicleta casi nueva',
+      price: 356,
+      image: '../assets/img/bike.jpg'
+    },
+    {
+      id: '3',
+      name: 'Colleción de albumnes',
+      price: 34,
+      image: '../assets/img/album.jpg'
+    },
+    {
+      id: '4',
+      name: 'Mis libros',
+      price: 23,
+      image: '../assets/img/books.jpg'
+    },
+  ];
 
   // Link img: https://www.w3schools.com/howto/img_avatar.png
 
