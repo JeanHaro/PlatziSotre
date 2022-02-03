0855ed087bee86102cdfafcaf17a848976751d42
Jean Haro <JeanCHL98@hotmail.com>
2022-02-03 01:01:49 -0500

    Uso de Inputs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-store';
+  imgParent = '';
+
+  // Link img: https://www.w3schools.com/howto/img_avatar.png
 }
