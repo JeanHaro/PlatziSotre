d238b0e8d837edecb7ec2059287afbda35757ae7
Jean Haro <JeanCHL98@hotmail.com>
2022-02-23 17:43:52 -0500

    Evitando doble subscribe

--- src/app/pages/category/category.component.ts
+++ src/app/pages/category/category.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 // ActivatedRoute - Leer parámetros desde el routing
 import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
 
 // Servicios
 import { ProductsService } from '../../services/products.service';
@@ -11,6 +12,8 @@ import { Product } from '../../models/product.model';
 
 @Component({
   selector: 'app-category',
+  // Si el template del HTML tenemos una sola línea se puede hacer así también 
+  /* template: `<app-products [products]="products"></app-products>`, */
   templateUrl: './category.component.html',
   styleUrls: ['./category.component.scss']
 })
@@ -29,18 +32,24 @@ export class CategoryComponent implements OnInit {
   ngOnInit(): void {
     // Suscripción a paramMap()
     this.route.paramMap
-      .subscribe(params => {
-        // Cual es el que queremos obtener, tiene que coincidir con el id
-        this.categoryId = params.get('id');
-        console.log(this.categoryId);
+      .pipe(
+        // switchMap - para evitar hacer un Callback hell
+        switchMap(params => {
+          // Cual es el que queremos obtener, tiene que coincidir con el id
+          this.categoryId = params.get('id');
+          // console.log(this.categoryId);
 
-        // Con el ID vamos hacer un request a la API 
-        if (this.categoryId) {
-          this.productsService.getByCategory(this.categoryId, this.limit, this.offset)
-            .subscribe(data => {
-              this.products = data;
-            })
-        }
+          // Con el ID vamos hacer un request a la API 
+          if (this.categoryId) {
+            return this.productsService.getByCategory(this.categoryId, this.limit, this.offset)
+          } 
+
+          // Si no tiene un id, manda un array vacio
+          return [];
+        })
+      )
+      .subscribe(data => {
+        this.products = data;
       });
   }
 }
